dd3953fecb7e0de2d51547b6b23811ccb9b35ede
Харакири Молима <kanikani.you@gmail.com>
2019-12-11 15:55:13 +0900

    Create let_translatable.py

---
+++ let_translatable.py
@@ -0,0 +1,18 @@
+import os
+import re
+import time
+if __name__ == '__main__':
+	for file_name in os.listdir("eng_txt_split"):
+		if file_name.endswith(".txt"):
+			print(file_name)
+			text = ""
+			with open("eng_txt_split/"+file_name) as f:
+				l = f.readlines()
+				for line in l:
+					text += str(line).rstrip('\n')
+				
+			print(text)
+			print("="*10)
+			path_w = "eng_txt/" + file_name
+			with open(path_w, mode='w') as f:
+				f.write(text)
